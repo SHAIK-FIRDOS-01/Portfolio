b4e8ccf81360bbd8c15faddf8dce697bcb35b995
SHAIK-FIRDOS-01 <shaikfirdos403@gmail.com>
2026-07-13 18:31:01 +0530

    fix: remove unused useRef in useMagnetic

--- src/hooks/useMagnetic.ts
+++ src/hooks/useMagnetic.ts
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react';
+import { useEffect } from 'react';
 import gsap from 'gsap';
 
 export default function useMagnetic(magnetic: React.RefObject<HTMLElement | null>) {
